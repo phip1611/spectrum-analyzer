89fda986fa44eedc56617f5c323ae2a7a667f324
Philipp Schuster <phip1611@gmail.com>
2021-03-28 16:44:33 +0200

    faster CI build

--- check-build.sh
+++ check-build.sh
@@ -1,8 +1,8 @@
 echo "checks that this builds on std+no_std + that all tests run"
 cargo build --all-targets
-cargo build --all-targets --release
+# cargo build --all-targets --release
 cargo test --all-targets
-cargo test --all-targets --release
+# cargo test --all-targets --release
 rustup target add thumbv7em-none-eabihf
 cargo check --target thumbv7em-none-eabihf --no-default-features --features "microfft-complex"
 cargo check --target thumbv7em-none-eabihf --no-default-features --features "microfft-real"
