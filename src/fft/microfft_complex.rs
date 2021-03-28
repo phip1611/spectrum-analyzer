240334667c1d26ab23234dc46c0f38f8efe2137e
Philipp Schuster <phip1611@gmail.com>
2021-03-28 11:49:26 +0200

    fixing embedded build (closes #15)

--- src/fft/microfft_complex.rs
+++ src/fft/microfft_complex.rs
@@ -93,8 +93,8 @@ impl Fft<FftResultType> for FftImpl {
     #[inline(always)]
     fn fft_map_result_to_f32(val: &FftResultType) -> f32 {
         // calculates sqrt(re*re + im*im), i.e. magnitude of complex number
-        let sum = (val.re * val.re + val.im * val.im);
-        let sqrt = sum.sqrt();
+        let sum = val.re * val.re + val.im * val.im;
+        let sqrt = libm::sqrtf(sum);
         debug_assert!(sqrt != f32::NAN, "sqrt is NaN!");
         sqrt
     }
@@ -102,5 +102,20 @@ impl Fft<FftResultType> for FftImpl {
     #[inline(always)]
     fn fft_calc_frequency_resolution(sampling_rate: u32, samples_len: u32) -> f32 {
         sampling_rate as f32 / samples_len as f32
+    }
+
+    #[inline(always)]
+    fn fft_relevant_res_samples_count(samples_len: usize) -> usize {
+        // See https://stackoverflow.com/a/4371627/2891595 for more information as well as
+        // https://www.gaussianwaves.com/2015/11/interpreting-fft-results-complex-dft-frequency-bins-and-fftshift/
+        //
+        // The indices 0 to N/2 (inclusive) are usually the most relevant. Although, index
+        // N/2-1 is declared as the last useful one on stackoverflow (because in typical applications
+        // Nyquist-frequency + above are filtered out), we include everything here.
+        // with 0..=(samples_len / 2) (inclusive) we get all frequencies from 0 to Nyquist theorem.
+        //
+        // Indices (samples_len / 2)..len() are mirrored/negative. You can also see this here:
+        // https://www.gaussianwaves.com/gaussianwaves/wp-content/uploads/2015/11/realDFT_complexDFT.png
+        samples_len / 2 + 1
     }
 }
