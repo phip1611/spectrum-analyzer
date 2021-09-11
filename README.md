89200b7c4f23a4b41269e0328ddf91df72b4db67
Philipp Schuster <phip1611@gmail.com>
2021-09-11 23:07:11 +0200

    Fix deprecated example in README (v1.0.1)

--- README.md
+++ README.md
@@ -42,12 +42,15 @@ features = ["rustfft-complex"] # or on of the other features
 ```rust
 use spectrum_analyzer::{samples_fft_to_spectrum, FrequencyLimit};
 use spectrum_analyzer::windows::hann_window;
+use spectrum_analyzer::scaling::divide_by_N;
 
+/// Minimal example.
 fn main() {
-    // This lib also works in `no_std` environments!
-    let samples: &[f32] = get_samples(); // TODO you need to implement the samples source
+    // YOU need to implement the samples source; get microphone input for example
+    let samples: &[f32] = &[0.0, 3.14, 2.718, -1.0, -2.0, -4.0, 7.0, 6.0];
     // apply hann window for smoothing; length must be a power of 2 for the FFT
-    let hann_window = hann_window(&samples[0..4096]);
+    // 2048 is a good starting point with 44100 kHz
+    let hann_window = hann_window(&samples[0..8]);
     // calc spectrum
     let spectrum_hann_window = samples_fft_to_spectrum(
         // (windowed) samples
@@ -56,11 +59,9 @@ fn main() {
         44100,
         // optional frequency limit: e.g. only interested in frequencies 50 <= f <= 150?
         FrequencyLimit::All,
-        // optional per element scaling function, e.g. `20 * log10(x)`; see doc comments
-        None,
-        // optional total scaling at the end; see doc comments
-        None,
-    );
+        // optional scale
+        Some(&divide_by_N),
+    ).unwrap();
 
     for (fr, fr_val) in spectrum_hann_window.data().iter() {
         println!("{}Hz => {}", fr, fr_val)
---
+++ examples/minimal.rs
@@ -0,0 +1,52 @@
+/*
+MIT License
+
+Copyright (c) 2021 Philipp Schuster
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+use spectrum_analyzer::scaling::divide_by_N;
+use spectrum_analyzer::windows::hann_window;
+use spectrum_analyzer::{samples_fft_to_spectrum, FrequencyLimit};
+
+/// Minimal example.
+fn main() {
+    // YOU need to implement the samples source; get microphone input for example
+    let samples: &[f32] = &[0.0, 3.14, 2.718, -1.0, -2.0, -4.0, 7.0, 6.0];
+    // apply hann window for smoothing; length must be a power of 2 for the FFT
+    // 2048 is a good starting point with 44100 kHz
+    let hann_window = hann_window(&samples[0..8]);
+    // calc spectrum
+    let spectrum_hann_window = samples_fft_to_spectrum(
+        // (windowed) samples
+        &hann_window,
+        // sampling rate
+        44100,
+        // optional frequency limit: e.g. only interested in frequencies 50 <= f <= 150?
+        FrequencyLimit::All,
+        // optional scale
+        Some(&divide_by_N),
+    )
+    .unwrap();
+
+    for (fr, fr_val) in spectrum_hann_window.data().iter() {
+        println!("{}Hz => {}", fr, fr_val)
+    }
+}
