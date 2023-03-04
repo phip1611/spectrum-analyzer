a1a3b4550da5692b4965d12ed0874dcb58e92e07
Philipp Schuster <phip1611@gmail.com>
2023-03-04 22:39:06 +0100

    lib.rs: code improvement

--- src/lib.rs
+++ src/lib.rs
@@ -173,7 +173,7 @@ pub fn samples_fft_to_spectrum(
     if samples.iter().any(|x| x.is_infinite()) {
         return Err(SpectrumAnalyzerError::InfinityValuesNotSupported);
     }
-    if !is_power_of_two(samples.len()) {
+    if !samples.len().is_power_of_two() {
         return Err(SpectrumAnalyzerError::SamplesLengthNotAPowerOfTwo);
     }
     let max_detectable_frequency = sampling_rate as f32 / 2.0;
@@ -365,26 +365,4 @@ fn complex_to_magnitude(val: &Complex32) -> f32 {
     let sqrt = libm::sqrtf(sum);
     debug_assert!(!sqrt.is_nan(), "sqrt is NaN!");
     sqrt
-}
-
-// idea from https://stackoverflow.com/questions/600293/how-to-check-if-a-number-is-a-power-of-2
-const fn is_power_of_two(num: usize) -> bool {
-    num != 0 && ((num & (num - 1)) == 0)
-}
-
-// tests module for small unit tests
-
-#[cfg(test)]
-mod tests2 {
-    use super::*;
-
-    #[test]
-    fn test_is_power_of_two() {
-        assert!(!is_power_of_two(0));
-        assert!(is_power_of_two(1));
-        assert!(is_power_of_two(2));
-        assert!(!is_power_of_two(3));
-        assert!(is_power_of_two(2));
-        assert!(is_power_of_two(256));
-    }
 }
