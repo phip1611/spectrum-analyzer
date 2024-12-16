f903d98cd7ab33a5457378fe216463cc7382e215
Philipp Schuster <phip1611@gmail.com>
2024-12-16 07:54:08 +0100

    nix: switch to flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "spectrum-analyzer Rust crate";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs =
+    { self, nixpkgs }@inputs:
+    {
+      devShells.x86_64-linux.default =
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+
+          # Mainly runtime deps of the examples, not the base lib.
+          runtimeDeps = with pkgs; [
+            alsa-lib
+            fontconfig
+            libxkbcommon
+            xorg.libXcursor
+            xorg.libX11
+          ];
+        in
+        pkgs.mkShell {
+          packages =
+            with pkgs;
+            [
+              pkg-config
+            ]
+            ++ runtimeDeps;
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath runtimeDeps}";
+        };
+    };
+}
