2eb8eb74dac8ce73c94182dddb0bcd8a6ee24612
Philipp Schuster <phip1611@gmail.com>
2024-12-17 10:59:24 +0100

    fft: remove expressive stack usage and prevent unnecessary copying
    Before, the data was copied into a stack array and then was collected
    in a Vector. Now, we only have the copying once. Miri ensures that the
    unsafe magic is fine.

--- src/fft.rs
+++ src/fft.rs
@@ -33,39 +33,36 @@ pub use microfft::Complex32;
 
 use alloc::vec::Vec;
 use core::convert::TryInto;
+use core::mem;
 use microfft::real;
 
-/// The result of a FFT is always complex but because different FFT crates might
-/// use different versions of "num-complex", each implementation exports
-/// it's own version that gets used in lib.rs for binary compatibility.
-pub use microfft::Complex32;
-
-/// Calculates the real FFT by invoking the proper function corresponding to the
-/// buffer length.
+/// Calculates the FFT by invoking the function of [`microfft::real`] that
+/// corresponds to the input size.
 macro_rules! real_fft_n {
     ($buffer:expr, $( $i:literal ),*) => {
         match $buffer.len() {
             $(
                 $i => {
-                    let mut buffer: [_; $i] = $buffer.try_into().unwrap();
+                    let fixed_size_view = $buffer.as_mut_slice().try_into().unwrap();
                     paste::paste! (
                         real::[<rfft_$i>]
-                    )(&mut buffer).to_vec()
+                    )(fixed_size_view)
                 }
             )*
-            _ => { unimplemented!("unexpected buffer len") }
+            _ => { unimplemented!("should be one of the supported buffer lengths, but was {}", $buffer.len()) }
         }
     };
 }
 
-/// Real FFT using [`microfft::real`].
+/// FFT using [`microfft::real`].
 pub struct FftImpl;
 
 impl FftImpl {
-    /// Calculates the FFT For the given input samples and returns a Vector of
-    /// of [`Complex32`] with length `samples.len() / 2 + 1`, where the first
-    /// index corresponds to the DC component and the last index to the Nyquist
-    /// frequency.
+    /// Calculates the FFT For the given input samples and returns a [`Vec`] of
+    /// [`Complex32`] with length `samples.len() / 2 + 1`.
+    ///
+    /// The first index corresponds to the DC component and the last index to
+    /// the Nyquist frequency.
     ///
     /// # Parameters
     /// - `samples`: Array with samples. Each value must be a regular floating
@@ -73,16 +70,72 @@ impl FftImpl {
     ///              a power of two. Otherwise, the function panics.
     #[inline]
     pub(crate) fn calc(samples: &[f32]) -> Vec<Complex32> {
-        let mut fft_res: Vec<Complex32> = real_fft_n!(
-            samples, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768
+        assert_eq!(
+            samples.len() % 2,
+            0,
+            "buffer length must be a multiple of two!"
         );
+        let mut vec_buffer = Vec::with_capacity(samples.len() + 2 /* Nyquist */);
+        assert_eq!(
+            vec_buffer.capacity() % 2,
+            0,
+            "vector capacity must be a multiple of two for safe casting!"
+        );
+
+        vec_buffer.extend_from_slice(samples);
+
+        // The result is a view into the buffer.
+        // We discard the view and directly operate on the buffer.
+        let _fft_res: &mut [Complex32] = real_fft_n!(
+            &mut vec_buffer,
+            2,
+            4,
+            8,
+            16,
+            32,
+            64,
+            128,
+            256,
+            512,
+            1024,
+            2048,
+            4096,
+            8192,
+            16384,
+            32768
+        );
+
+        // We transform the original vector while preserving its memory, to
+        // prevent any reallocation or unnecessary copying.
+        let mut buffer = {
+            let ptr = vec_buffer.as_mut_ptr().cast::<Complex32>();
+            let len = vec_buffer.len() / 2;
+            let capacity = vec_buffer.capacity() / 2;
+            let new_buffer_view = unsafe { Vec::from_raw_parts(ptr, len, capacity) };
+            mem::forget(vec_buffer);
+            new_buffer_view
+        };
 
         // `microfft::real` documentation says: the Nyquist frequency real value
         // is packed inside the imaginary part of the DC component.
-        let nyquist_fr_pos_val = fft_res[0].im;
-        fft_res[0].im = 0.0;
-        // manually add the nyquist frequency
-        fft_res.push(Complex32::new(nyquist_fr_pos_val, 0.0));
-        fft_res
+        let nyquist_fr_pos_val = buffer[0].im;
+        buffer[0].im = 0.0;
+        // manually add the Nyquist frequency
+        buffer.push(Complex32::new(nyquist_fr_pos_val, 0.0));
+        buffer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::fft::FftImpl;
+
+    /// This test is primarily for miri.
+    #[test]
+    fn test_memory_safety() {
+        let samples = [1.0, 2.0, 3.0, 4.0];
+        let fft = FftImpl::calc(&samples);
+
+        assert_eq!(fft.len(), 2 + 1);
     }
 }
