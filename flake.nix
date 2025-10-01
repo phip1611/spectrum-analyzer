35bbd198890ea923cae4d83a83adee917718a741
Philipp Schuster <phip1611@gmail.com>
2025-10-01 16:27:41 +0200

    flake: update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "spectrum-analyzer Rust crate";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs =
